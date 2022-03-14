bf142c19a4f2f33541f38fa08c7794ea1592cec0
Mediavilla85 <54158084+Mediavilla85@users.noreply.github.com>
2022-03-14 12:03:25 +0100

    Delete FullCRUDapp directory

--- FullCRUDapp/target/classes/import.sql
+++
@@ -1,9 +0,0 @@
-/* Populate tabla clientes*/
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alba', 'varela','albatepego@tereviento.bum', '2017/12/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Joseba', 'Portas','takemyMoney@bitch.now', '2017/12/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alberto', 'moreno','heavy@metal.frk', '2017/12/01');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ivan', 'terrible','rusos@miedo.dan', '2017/12/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Juri', 'Gargarin','vodka@space.ext', '2017/2/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Adrian', 'Zamora','tiogilito@gmail.com', '2017/12/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('German', 'sabedios','exterminarpaletos@gmail.com', '2017/1/11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Carlos', 'Peleteiro','Ruzo@hotmail.com', '2017/12/11');
